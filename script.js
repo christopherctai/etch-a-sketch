e03d329b727d8f4560eef0b3e018f86d1fabd721
Chris Tai <christopherctai@gmail.com>
2023-01-12 16:33:10 -0800

    Add event listener to each grid item to respond to hovers

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let numberOfSquares = 2;
+let numberOfSquares = 5;
 const grid = document.querySelector('.grid-container');
 
 
@@ -10,5 +10,16 @@ grid.style.gridTemplateColumns = `repeat(${numberOfSquares}, 1fr)`;
 
 for (let i = 1; i <= squareNumber(numberOfSquares); i++) {
     let square = document.createElement('div');
+    square.classList.add('grid-item');
+
     grid.appendChild(square);
 } 
+
+let gridItems = document.querySelectorAll('.grid-item');
+
+gridItems.forEach((gridItem), () => {
+    gridItem.addEventListener('hover', () => {
+        gridItem.style.backgroundColor = "white"; 
+    });
+});
+
